3b6534eb2fc0824f0133adc987fee70e159c4a44
Boaz Jan <boaz.jan@gmail.com>
2024-12-18 22:42:49 +0200

    dedup function for table list

--- lua/utils/tables.lua
+++ lua/utils/tables.lua
@@ -36,4 +36,19 @@ function M.map_entries(tbl, f)
   return t
 end
 
+---@generic T
+---@param list T[]
+---@return T[]
+function M.dedup(list)
+  local ret = {}
+  local seen = {}
+  for _, v in ipairs(list) do
+    if not seen[v] then
+      table.insert(ret, v)
+      seen[v] = true
+    end
+  end
+  return ret
+end
+
 return M
